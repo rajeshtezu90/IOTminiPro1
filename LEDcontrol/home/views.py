b41374f5c51c7a9482eff905781373df252292be
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-05-21 00:13:41 +0000

    added raspberry gpio functionality to glow LED

---
+++ LEDcontrol/home/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import RPi.GPIO as gpio
+
+# Create your views here.
+
+def index(request):
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(11, gpio.OUT)
+    gpio.setup(11, gpio.LOW)
+    selected = request.POST.get('led', False)
+
+    if selected == 'on':
+        gpio.output(11, True)
+    else:
+        gpio.output(11, False)
+        
+    context = {'selected': selected,}
+    return render(request, 'home/index.html', context)
+    #return HttpResponse('<h1> welcome </h1>')
+    
